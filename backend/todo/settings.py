ed5a734ab16765fdd446bd4362b0a6309c097b64
Sebastian L <lopez.sebastian@correounivalle.edu.co>
2020-08-12 15:51:32 -0500

    Adding integration with mysql

--- backend/todo/settings.py
+++ backend/todo/settings.py
@@ -78,13 +78,24 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
+    
+} """
 
+DATABASES = {
+'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'docker',
+    'USER': 'docker',
+    'PASSWORD': 'docker', 
+    'HOST': 'db',
+    'PORT': 3306,
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
