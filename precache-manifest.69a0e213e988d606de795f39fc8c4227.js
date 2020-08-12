55a4d5af7d23268688bfc6c966d6d88022736e9d
Sebastian L <lopez.sebastian@correounivalle.edu.co>
2020-08-12 16:20:54 -0500

    Updates

---
+++ precache-manifest.69a0e213e988d606de795f39fc8c4227.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35193f9611ab170550887b0fe1fce518",
+    "url": "/todo-test/index.html"
+  },
+  {
+    "revision": "ac893407ebb1a72ba5eb",
+    "url": "/todo-test/static/css/main.adc7f077.chunk.css"
+  },
+  {
+    "revision": "2b3a4fdf208fcb18053d",
+    "url": "/todo-test/static/js/2.4901d9d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-test/static/js/2.4901d9d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac893407ebb1a72ba5eb",
+    "url": "/todo-test/static/js/main.35fa5560.chunk.js"
+  },
+  {
+    "revision": "2428213a8b693d1d0189",
+    "url": "/todo-test/static/js/runtime-main.8f347b58.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/todo-test/precache-manifest.69a0e213e988d606de795f39fc8c4227.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todo-test/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
